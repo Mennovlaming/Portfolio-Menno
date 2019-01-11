af8f95b49ea4c5bd07c95eb9bfd0cb49950554c7
Menno <Mennovlaming@gmail.com>
2019-01-11 16:17:31 +0100

    sitetitel

--- config/website.js
+++ config/website.js
@@ -3,10 +3,10 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Menno Vlaming', // Navigation and Site Title
+  siteTitleAlt: 'MV', // Alternative Site title for SEO
+  siteTitleShort: 'MV', // short_name for manifest
+  siteUrl: 'https://mennovlaming.nl', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
   siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
